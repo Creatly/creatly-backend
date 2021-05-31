018086ca8ed74c6fc2bd4cf70672e9658c15c6aa
Alexander Melentyev <alexander.melentyev@icloud.com>
2021-05-31 09:29:25 +0300

    Add JWT

--- tests/admins_test.go
+++ tests/admins_test.go
@@ -1,12 +1,15 @@
 package tests
 
 import (
+	"context"
 	"encoding/json"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 
 	"github.com/gin-gonic/gin"
+	"github.com/zhashkevych/creatly-backend/internal/domain"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 func (s *APITestSuite) TestAdminGetAllCourses() {
@@ -14,8 +17,26 @@ func (s *APITestSuite) TestAdminGetAllCourses() {
 	s.handler.Init(router.Group("/api"))
 	r := s.Require()
 
+	id := primitive.NewObjectID()
+	studentEmail, password := "test4@test.com", "qwerty123"
+	passwordHash, err := s.hasher.Hash(password)
+	s.NoError(err)
+
+	_, err = s.db.Collection("students").InsertOne(context.Background(), domain.Student{
+		ID:           id,
+		Email:        studentEmail,
+		Password:     passwordHash,
+		SchoolID:     school.ID,
+		Verification: domain.Verification{Verified: true},
+	})
+	s.NoError(err)
+
+	jwt, err := s.getJwt(id)
+	s.NoError(err)
+
 	req, _ := http.NewRequest("GET", "/api/v1/admins/courses", nil)
 	req.Header.Set("Content-type", "application/json")
+	req.Header.Set("Authorization", "Bearer "+jwt)
 
 	resp := httptest.NewRecorder()
 	router.ServeHTTP(resp, req)
