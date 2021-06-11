dbb48ba53192e9ddd78302a749c853c708d74070
Alexander Melentyev <alexander.melentyev@icloud.com>
2021-06-11 20:44:12 +0300

    Delete duplicated errors

--- internal/service/students.go
+++ internal/service/students.go
@@ -71,8 +71,8 @@ func (s *StudentsService) SignUp(ctx context.Context, input StudentSignUpInput) 
 	}
 
 	if err := s.repo.Create(ctx, student); err != nil {
-		if errors.Is(err, repository.ErrUserAlreadyExists) {
-			return ErrUserAlreadyExists
+		if errors.Is(err, domain.ErrUserAlreadyExists) {
+			return err
 		}
 
 		return err
@@ -100,8 +100,8 @@ func (s *StudentsService) SignIn(ctx context.Context, input SchoolSignInInput) (
 
 	student, err := s.repo.GetByCredentials(ctx, input.SchoolID, input.Email, passwordHash)
 	if err != nil {
-		if errors.Is(err, repository.ErrUserNotFound) {
-			return Tokens{}, ErrUserNotFound
+		if errors.Is(err, domain.ErrUserNotFound) {
+			return Tokens{}, err
 		}
 
 		return Tokens{}, err
